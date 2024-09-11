b1913eb47951541c5c88e87043d2cef298dd8ebd
Adam-Dowsett <78181424+Adam-Dowsett@users.noreply.github.com>
2024-09-11 21:58:40 +0100

    updated the personal summary of myself on the home page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,13 +10,32 @@ export default async function Home() {
       <h1 className="text-7xl font-extrabold">
         Hello I&apos;m <span className="text-blue-500">Adam</span>!
       </h1>
-      <p className="mt-3 text-xl dark:text-gray-400 light:text-gray-700">
-        Greetings and welcome to my website! I&apos;m excited to share my
-        projects with you and hope you&apos;ll find them interesting and
-        inspiring. If you have any questions or would like to collaborate,
-        please feel free to connect with me on LinkedIn or Github using the
-        links below. Thanks for stopping by!
-      </p>
+
+      <div className="flex flex-col gap-1">
+        <p className="mt-3 text-sm dark:text-gray-400 light:text-gray-700">
+          I’m Adam, a software engineer at a dynamic fintech startup, where we
+          are revolutionizing the way financial analysts, buy and sell-side
+          professionals, and investor relations officers interact with data and
+          insights. Our cutting-edge solutions are designed to empower users
+          with intuitive tools that deliver powerful results.
+        </p>
+        <p className="mt-3 text-sm dark:text-gray-400 light:text-gray-700">
+          My work focuses on building seamless integrations between widely-used
+          platforms, such as Excel and advanced web-based analytics. I’ve
+          developed Excel add-ins using the JavaScript API, creating a smooth
+          transition from spreadsheet-based workflows to advanced analytics on
+          our platform. This ensures users can leverage the full capabilities of
+          our product while staying within the familiarity of Excel.
+        </p>
+        <p className="mt-3 text-sm dark:text-gray-400 light:text-gray-700">
+          I’ve also integrated HubSpot CRM, enabling our sales team to tap into
+          the full potential of HubSpot’s robust features, streamlining their
+          processes and improving efficiency. On the technical front, I
+          specialize in front-end development with React, alongside Next.js, and
+          handle backend development using Node.js and Bun, ensuring our
+          products are fast, scalable, and user-friendly.
+        </p>
+      </div>
       <h2 className="mt-8 font-bold dark:text-gray-400 light:text-gray-700 text-3xl text-center">
         My Projects
       </h2>
